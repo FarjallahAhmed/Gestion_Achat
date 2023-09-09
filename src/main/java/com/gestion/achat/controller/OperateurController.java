049bc73473b47d9f80e8d39a9bf28bb8321edd96
FarjallahAhmed <ahmed.farjallah@esprit.tn>
2023-09-09 19:34:22 +0100

    Create the Operateur Controller

---
+++ src/main/java/com/gestion/achat/controller/OperateurController.java
@@ -0,0 +1,54 @@
+package com.gestion.achat.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gestion.achat.entity.Operateur;
+import com.gestion.achat.service.OperateurService;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
+public class OperateurController {
+
+	
+	@Autowired
+	OperateurService operateurService;
+	
+	
+	@GetMapping("retrieve-all-operateur")
+	public List<Operateur> getAllOperateur(){
+		return operateurService.retrieveAllOperateurs();
+	}
+	
+	@PostMapping("add-operateu")
+	public Operateur addOperateur(@RequestBody Operateur operateur) {
+		return operateurService.addOperateur(operateur);
+	}
+	
+	@PutMapping("update-operateur")
+	public Operateur updateOperateur(@RequestBody Operateur operateur) {
+		return operateurService.upOperateur(operateur);
+	}
+	
+	@GetMapping("retrieve-operateur/{id}")
+	public Operateur getOperateur(@PathParam("id") Long id) {
+		return operateurService.retrieveOperateur(id);
+	}
+	
+	@DeleteMapping("delete-operateur/{id}")
+	public void deleteOperateur(@PathParam("id") Long id) {
+		operateurService.removeOperateur(id);
+	}
+	
+	
+	
+	
+}
